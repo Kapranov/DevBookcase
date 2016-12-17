832716d439728d510e7005f3c7ed007985d6919f
Oleg G.Kapranov <oleg@vertiopia.com>
2016-12-17 17:35:21 +0200

    created empty backend & frontend

---
+++ bookcase-api/config/application.rb
@@ -0,0 +1,17 @@
+require_relative 'boot'
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "rails/test_unit/railtie"
+
+Bundler.require(*Rails.groups)
+
+module BookcaseApi
+  class Application < Rails::Application
+    config.api_only = true
+  end
+end
